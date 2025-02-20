92ea73e88bf3efd765f896ee55b058db3252a4a6
esthertozzo <tozzo.est@gmail.com>
2025-02-20 11:41:18 -0300

    ajuste da ac01

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,6 +15,17 @@ export class HomePage {
   grausFahrenheit = "";
   FahrenheitValor = "";
   CelsiusValor = "";
+  lado1 = "";
+  lado2 = "";
+  lado3 = "";
+  triangulo = "";
+  nota1 = "";
+  nota2 = "";
+  nota3 = "";
+  nota4 = "";
+  valorMedia = "";
+  definicaoMedia = "";
+
 
   // ex 1
   // metodo
@@ -27,4 +38,38 @@ export class HomePage {
     this.CelsiusValor = ((parseFloat(this.grausFahrenheit)- 32)*(5/9)).toString();
 
   }
+
+   // ex 2
+   verificarTriangulo(){
+    if(this.lado1 < this.lado2 + this.lado3 && this.lado2 < this.lado1 + this.lado3 && this.lado3 < this.lado1 + this.lado2){
+      if(this.lado1==this.lado2 && this.lado2==this.lado3 && this.lado1==this.lado3){
+        this.triangulo = ("É um tringulo equilátero");
+      }
+      else if(this.lado1!=this.lado2 && this.lado2!=this.lado3 && this.lado1!=this.lado3){
+        this.triangulo = ("É um triângulo escaleno");
+      }else{
+        this.triangulo = ("É um triângulo isosceles");
+      }
+    }else{
+      this.triangulo = ("Os lados informados não formam um triângulo!");
+  }
+  }
+
+  // ex 3
+  media(){
+    this.valorMedia = (((parseFloat(this.nota1)) + (parseFloat(this.nota2)) + (parseFloat(this.nota3)) + (parseFloat(this.nota4)))/ 4).toString();
+
+    if(Number(this.valorMedia) < 3){
+      this.definicaoMedia = (`Sua média é ${this.valorMedia}! Você está retido.`);
+    }
+    else if(Number(this.valorMedia) >= 3 && Number(this.valorMedia) <=5.9 ){
+       this.definicaoMedia = (`Sua média é ${this.valorMedia}! Você está de recuperação :(`);
+    }
+    else{
+      this.definicaoMedia = (`Sua média é ${this.valorMedia}! Você está de aprovado, parabéns (:`);
+    }
+
+  }
+
+
 }
